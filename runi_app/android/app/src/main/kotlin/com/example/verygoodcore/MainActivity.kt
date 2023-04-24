1431383cd67d6e9f555763dccb1ff1a9f2df5ec8
Mike <michael.chevallier@gmail.com>
2023-04-24 15:03:25 +0200

    initial commit

---
+++ runi_app/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.runi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ runi_app/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:runi_app/app/app.dart';
+import 'package:runi_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
